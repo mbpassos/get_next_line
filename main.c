e717aa823c5f6a5d27d4a3dccb93270b7ef71aa7
mbpassos <mbpassos@HP>
2023-08-30 13:54:31 +0100

    feat:update main file

--- main.c
+++ main.c
@@ -1,36 +1,36 @@
 // #include "get_next_line_bonus.h"
-#include "get_next_line.h"
+// #include "get_next_line.h"
 
 // FOR MANDATORY PART
-int main(void)
-{
-    int fd1, fd2;
-    char *line;
-    fd1 = open("file1.txt", O_RDONLY); // Open file1 for reading
-    fd2 = open("file2.txt", O_RDONLY); // Open file2 for reading
-    if (fd1 == -1 || fd2 == -1)
-    {
-        perror("Error opening file");
-        return 1;
-    }
-    // Reading lines from file1
-    printf("Reading lines from file1:\n");
-    while ((line = get_next_line(fd1)) != NULL)
-    {
-        printf("File 1 line: %s\n", line);
-        free(line);
-    }
-    // Reading lines from file2
-    printf("\nReading lines from file2:\n");
-    while ((line = get_next_line(fd2)) != NULL)
-    {
-        printf("File 2 line: %s\n", line);
-        free(line);
-    }
-    close(fd1); // Close file1
-    close(fd2); // Close file2
-    return 0;
-}
+// int main(void)
+// {
+//     int fd1, fd2;
+//     char *line;
+//     fd1 = open("file1.txt", O_RDONLY); // Open file1 for reading
+//     fd2 = open("file2.txt", O_RDONLY); // Open file2 for reading
+//     if (fd1 == -1 || fd2 == -1)
+//     {
+//         perror("Error opening file");
+//         return 1;
+//     }
+//     // Reading lines from file1
+//     printf("Reading lines from file1:\n");
+//     while ((line = get_next_line(fd1)) != NULL)
+//     {
+//         printf("File 1 line: %s\n", line);
+//         free(line);
+//     }
+//     // Reading lines from file2
+//     printf("\nReading lines from file2:\n");
+//     while ((line = get_next_line(fd2)) != NULL)
+//     {
+//         printf("File 2 line: %s\n", line);
+//         free(line);
+//     }
+//     close(fd1); // Close file1
+//     close(fd2); // Close file2
+//     return 0;
+// }
 
 // FOR BONUS
 // int main(void)
@@ -73,48 +73,55 @@ int main(void)
 // }
 
 // TO SHOW DIFFERENCES BETWEEN FUNCTIONS
-// int main()
-// {
+// int main(void) {
 //     int fd1, fd2;
 //     char *line;
+    
 //     // Open the first file
 //     fd1 = open("file1.txt", O_RDONLY);
-//     if (fd1 < 0)
-//     {
+//     if (fd1 < 0) {
 //         perror("Error opening file1.txt");
-//         return 1;
+//         return 0;
 //     }
+    
 //     // Open the second file
 //     fd2 = open("file2.txt", O_RDONLY);
-//     if (fd2 < 0)
-//     {
+//     if (fd2 < 0) {
 //         perror("Error opening file2.txt");
-//         return 1;
+//         return 0;
 //     }
-//     // Read lines from both files using get_next_line_bonus
+    
 //     printf("Reading from file1.txt and file2.txt using get_next_line_bonus:\n");
-//     int files_open = 2;
-//     while (files_open > 0)
-//     {
-//         for (int fd = 0; fd <= MAX_FILES; fd++)
-//         {
-//             if (fd == fd1 || fd == fd2)
-//             {
-//                 line = get_next_line(fd);
-//                 if (line)
-//                 {
-//                     printf("[%d] %s\n", fd, line);
-//                     free(line);
-//                 }
-//                 else
-//                 {
-//                     // File has reached the end or an error occurred
-//                     files_open--;
-//                 }
-//             }
+    
+//     // Continue reading until both files have reached the end
+//     while (1) {
+//         line = get_next_line(fd1);
+//         if (line) {
+//             printf("[%d] %s\n", fd1, line);
+//             free(line);
+//         }
+//         else {
+//             // File 1 has reached the end or an error occurred
+//             close(fd1);
+//             fd1 = -1; // Mark as closed
+//         }
+        
+//         line = get_next_line(fd2);
+//         if (line) {
+//             printf("[%d] %s\n", fd2, line);
+//             free(line);
+//         }
+//         else {
+//             // File 2 has reached the end or an error occurred
+//             close(fd2);
+//             fd2 = -1; // Mark as closed
+//         }
+        
+//         // Break the loop if both files have reached the end
+//         if (fd1 == -1 && fd2 == -1) {
+//             break;
 //         }
 //     }
-//     close(fd1);
-//     close(fd2);
+    
 //     return 0;
 // }
